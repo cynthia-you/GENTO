a6414a02ae40b5bfca54cf5f6fff04d4266eafd7
cynthia-you <37206493+cynthia-you@users.noreply.github.com>
2026-05-19 11:40:07 +0800

    update c example

--- GENTO_SDK/C_EXAMPLE/example_basic_Brake.cpp
+++ GENTO_SDK/C_EXAMPLE/example_basic_Brake.cpp
@@ -1,68 +1,100 @@
-#include "L1Robot/L1Robot.h"
-#include "Common/FXCommon.h"
-#include "Common/FXType.h"
-#include "Common/FXErrorCode.h"
-#include <windows.h>
+/**
+ * @file example_basic_Brake.cpp
+ * @brief Example demonstrating how to control the brakes of L1 robot arms.
+ *
+ * This example shows how to:
+ * - Query SDK and controller versions
+ * - Check and recover the robot arm state (IDLE / ERROR)
+ * - Unlock and lock all brakes of a specified robot arm
+ *
+ * @warning Releasing the brakes may cause the arm to drop due to gravity.
+ *          Ensure the arm is held securely before unlocking the brakes.
+ */
 
+#include "L1Robot.h"
 
+/**
+ * @brief Entry point of the brake control example.
+ *
+ * Workflow overview:
+ * 1. Initialize communication with the robot controller
+ * 2. Retrieve SDK and controller versions
+ * 3. Verify the arm state and recover to IDLE if necessary
+ * 4. Unlock all brakes of ARM0
+ * 5. Lock all brakes of ARM0
+ *
+ * @param[in] argc Argument count (unused)
+ * @param[in] argv Argument vector (unused)
+ * @return int Exit code (0 on normal exit)
+ */
 int main(int argc, char** argv)
 {
-	int sdk_version = 0;
-	int controller_version = 0;
-	unsigned int system_errorcode = 0;
-	FXStateType obj_state = FX_STATE_UNKNOWN;
-	
-	sdk_version = FX_L1_System_GetSDKVersion();
-	printf("SDK version is 0x%08x\n", sdk_version);
+    int sdk_version = 0;                  ///< SDK version number
+    int controller_version = 0;           ///< Controller firmware version
+    unsigned int system_errorcode = 0;    ///< Last system error code
+    FXStateType obj_state = FX_STATE_UNKNOWN;
 
-	if (FX_L1_System_Link(6, 6, 7, 190, FX_LOG_ALL_FLAG) < 0)
-	{
-		printf("Failed to link system\n");
-		goto WAIT_EXIT;
-	}
+    /* Get SDK version */
+    sdk_version = FX_L1_System_GetSDKVersion();
+    printf("SDK version is 0x%08x\n", sdk_version);
 
-	controller_version = FX_L1_System_GetControllerVersion();
-	printf("Controller version is 0x%08x\n", controller_version);
+    /* Establish communication with the controller */
+    if (FX_L1_System_Link(6, 6, 7, 190, FX_LOG_ALL_FLAG) < 0)
+    {
+        printf("Failed to link system\n");
+        goto WAIT_EXIT;
+    }
 
-	obj_state = FX_L1_Fbk_CurrentState(FX_OBJ_ARM0);
-	if (obj_state == FX_STATE_ERROR)
-	{
-		printf("Brake lock/unlock must be run in FX_STATE_IDLE state, try to reset error\n");
-		if (FX_L1_State_ResetError(FX_OBJ_ARM0, 1000, &system_errorcode) == FUNC_RET_SUCCESS)
-		{
-			printf("Reset arm0 error success, arm0 is now in STATE_IDLE state\n");
-		}
-		else
-		{
-			printf("Failed to reset arm0 error, errorcode = 0x%08x\n", system_errorcode);
-			goto WAIT_EXIT;
-		}
-	}
-	else if (obj_state != FX_STATE_IDLE)
-	{
-		if (FX_L1_State_SwitchToIdle(FX_OBJ_ARM0, 1000) != FUNC_RET_SUCCESS)
-		{
-			printf("Failed to transfer arm0 to STATE_IDLE state\n");
-			goto WAIT_EXIT;
-		}
-	}
-	
-	printf("Press any key to unlock all brakes of arm0, please hold the arm carefully or it will drop!\n");
-	getchar();
-	if (FX_L1_Config_SetBrakeUnlock(FX_OBJ_ARM0, 0xFF) != FUNC_RET_SUCCESS)
-	{
-		printf("Unlock all brakes of arm0 failed\n");
-	}
+    /* Get controller version */
+    controller_version = FX_L1_System_GetControllerVersion();
+    printf("Controller version is 0x%08x\n", controller_version);
 
-	printf("Press any key to lock all brakes of arm0\n");
-	getchar();
-	if (FX_L1_Config_SetBrakeLock(FX_OBJ_ARM0, 0xFF) != FUNC_RET_SUCCESS)
-	{
-		printf("Lock all brakes of arm0 failed\n");
-	}
+    /*
+     * Check current arm state.
+     * Brake operations are only allowed in FX_STATE_IDLE.
+     */
+    obj_state = FX_L1_Fbk_CurrentState(FX_OBJ_ARM0);
+    if (obj_state == FX_STATE_ERROR)
+    {
+        printf("Brake lock/unlock must be run in FX_STATE_IDLE state, try to reset error\n");
+        if (FX_L1_State_ResetError(FX_OBJ_ARM0, 1000, &system_errorcode) == FUNC_RET_SUCCESS)
+        {
+            printf("Reset arm0 error success, arm0 is now in STATE_IDLE state\n");
+        }
+        else
+        {
+            printf("Failed to reset arm0 error, errorcode = 0x%08x\n", system_errorcode);
+            goto WAIT_EXIT;
+        }
+    }
+    else if (obj_state != FX_STATE_IDLE)
+    {
+        if (FX_L1_State_SwitchToIdle(FX_OBJ_ARM0, 1000) != FUNC_RET_SUCCESS)
+        {
+            printf("Failed to transfer arm0 to STATE_IDLE state\n");
+            goto WAIT_EXIT;
+        }
+    }
+
+    /* Unlock all brakes of ARM0 */
+    printf("Press any key to unlock all brakes of arm0, "
+           "please hold the arm carefully or it will drop!\n");
+    getchar();
+    if (FX_L1_Config_SetBrakeUnlock(FX_OBJ_ARM0, 0xFF) != FUNC_RET_SUCCESS)
+    {
+        printf("Unlock all brakes of arm0 failed\n");
+    }
+
+    /* Lock all brakes of ARM0 */
+    printf("Press any key to lock all brakes of arm0\n");
+    getchar();
+    if (FX_L1_Config_SetBrakeLock(FX_OBJ_ARM0, 0xFF) != FUNC_RET_SUCCESS)
+    {
+        printf("Lock all brakes of arm0 failed\n");
+    }
 
 WAIT_EXIT:
-	printf("Press any key to exit\n");
-	getchar();
-	return 0;
+    printf("Press any key to exit\n");
+    getchar();
+    return 0;
 }
--- GENTO_SDK/C_EXAMPLE/example_basic_LinkSystem.cpp
+++ GENTO_SDK/C_EXAMPLE/example_basic_LinkSystem.cpp
@@ -1,33 +1,61 @@
-#include "L1Robot/L1Robot.h"
-#include "Common/FXCommon.h"
-#include "Common/FXType.h"
-#include "Common/FXErrorCode.h"
-#include <windows.h>
+/**
+ * @file example_basic_LinkSystem.cpp
+ * @brief Example demonstrating how to establish and terminate communication
+ *        with the L1 robot controller system.
+ *
+ * This example shows how to:
+ * - Query the SDK version
+ * - Establish a communication link with the robot controller
+ * - Retrieve the controller firmware version
+ * - Safely disconnect from the controller
+ *
+ * This is the most fundamental example and should be executed before
+ * any other robot control examples.
+ */
 
+#include "L1Robot.h"
+
+/**
+ * @brief Entry point of the system link example.
+ *
+ * Workflow overview:
+ * 1. Retrieve the SDK version
+ * 2. Establish communication with the robot controller
+ * 3. Retrieve the controller firmware version
+ * 4. Disconnect from the controller
+ *
+ * @param[in] argc Argument count (unused)
+ * @param[in] argv Argument vector (unused)
+ * @return int Exit code (0 on normal exit)
+ */
 int main(int argc, char** argv)
 {
-	int sdk_version = 0;
-	int controller_version = 0;
-	
-	sdk_version = FX_L1_System_GetSDKVersion();
-	printf("SDK version is 0x%08x\n", sdk_version);
+    int sdk_version = 0;                  ///< SDK version number
+    int controller_version = 0;           ///< Controller firmware version
 
-	if (FX_L1_System_Link(6, 6, 7, 190, FX_LOG_ALL_FLAG) < 0)
-	{
-		printf("Failed to link system\n");
-		goto WAIT_EXIT;
-	}
+    /* Get SDK version */
+    sdk_version = FX_L1_System_GetSDKVersion();
+    printf("SDK version is 0x%08x\n", sdk_version);
 
-	controller_version = FX_L1_System_GetControllerVersion();
-	printf("Controller version is 0x%08x\n", controller_version);
+    /* Establish communication with the controller */
+    if (FX_L1_System_Link(6, 6, 7, 190, FX_LOG_ALL_FLAG) < 0)
+    {
+        printf("Failed to link system\n");
+        goto WAIT_EXIT;
+    }
 
-	printf("Press any key to unlink system...\n");
-	getchar();
-	FX_L1_System_Unlink();
-	printf("Unlink system\n");
+    /* Get controller version */
+    controller_version = FX_L1_System_GetControllerVersion();
+    printf("Controller version is 0x%08x\n", controller_version);
+
+    /* Disconnect from the controller */
+    printf("Press any key to unlink system...\n");
+    getchar();
+    FX_L1_System_Unlink();
+    printf("Unlink system\n");
 
 WAIT_EXIT:
-	printf("Press any key to exit\n");
-	getchar();
-	return 0;
+    printf("Press any key to exit\n");
+    getchar();
+    return 0;
 }
